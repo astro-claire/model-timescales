88b277b86199a60c34289090a2df6aea9265b6fa
astro-claire <clairewilliams@g.ucla.edu>
2025-08-22 13:44:07 -0700

    added escape velocity

--- src/timescales/utils/__init__.py
+++ src/timescales/utils/__init__.py
@@ -1,8 +1,9 @@
 from .units import as_quantity
-from .energy import kinetic_energy, gravitational_potential_energy
+from .energy import kinetic_energy, gravitational_potential_energy, escape_velocity
 
 __all__= [
     "as_quantity",
     "kinetic_energy",
     "gravitational_potential_energy"
+    "escape_velocity"
 ]
