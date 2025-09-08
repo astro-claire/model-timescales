fc59274810ac3fe987bf707382dcf8cdd4cc5f48
astro-claire <clairewilliams@g.ucla.edu>
2025-09-08 14:58:45 -0700

    added filtering utility

--- src/timescales/utils/__init__.py
+++ src/timescales/utils/__init__.py
@@ -1,9 +1,11 @@
 from .units import as_quantity
 from .energy import kinetic_energy, gravitational_potential_energy, escape_velocity
+from .filtering import filter_kwargs_for
 
 __all__= [
     "as_quantity",
     "kinetic_energy",
     "gravitational_potential_energy"
-    "escape_velocity"
+    "escape_velocity",
+    "filter_kwargs_for"
 ]
