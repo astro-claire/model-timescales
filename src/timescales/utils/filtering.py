317131769829bf82cf3cf90f1033dc50384cc81e
astro-claire <clairewilliams@g.ucla.edu>
2025-09-08 14:58:36 -0700

    Initial commit

---
+++ src/timescales/utils/filtering.py
@@ -0,0 +1,6 @@
+src/timescales/utils/filtering.py
+
+def filter_kwargs_for(func, kwargs):
+    sig = inspect.signature(func)
+    valid_keys = set(sig.parameters.keys())
+    return {k: v for k, v in kwargs.items() if k in valid_keys}
