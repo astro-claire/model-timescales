99531570baaea1e2348d28f101b11b6d667a7c40
astro-claire <clairewilliams@g.ucla.edu>
2025-09-16 10:39:38 -0700

    Added local dynamical friction timescale

--- src/timescales/physics/dynamical_friction.py
+++ src/timescales/physics/dynamical_friction.py
@@ -5,6 +5,7 @@ from astropy.units import Quantity
 import astropy.constants as c
 from ..utils import as_quantity
 from .registry import register_timescale
+from .relaxation import relaxation_timescale
 
 @register_timescale("t_df_b", aliases=("bulk_dynamical-friction",))
 def bulk_dynamical_friction_timescale(M_cl,M_BH,r_h,*, 
@@ -24,3 +25,17 @@ def bulk_dynamical_friction_timescale(M_cl,M_BH,r_h,*,
     r_h = as_quantity(r_h, radius_units)
     return 20 * u.Myr * (20.*u.Msun/ M_BH) * (M_cl/ (1e5 *u.Msun))**0.5 * (r_h/(1*u.pc))**(3./2.)
 
+@register_timescale("t_df", aliases = ("dynamical_friction"))
+def dynamical_friction_timescale(M_obj, Mstar_cluster,v, rho, mass,*,
+                                    coulomb=10, 
+                                    v_unit = u.km/u.s, 
+                                    rho_unit = u.g/u.cm**3,
+                                    mass_unit = u.Msun):
+    """ 
+    Dynamical friction timescale at a given radius (local) 
+    """
+    M_obj = as_quantity(M_obj, mass_unit)
+    Mstar_cluster = as_quantity(Mstar_cluster, mass_unit)
+    massratio = Mstar_cluster/M_obj
+    t_relax = relaxation_timescale(v,rho, mass, coulomb=10, v_unit = u.km/u.s, rho_unit = u.g/u.cm**3,mass_unit = u.Msun)
+    return t_relax * massratio
