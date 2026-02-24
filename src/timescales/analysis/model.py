c2436153a84f7dd364ed2a5928353c6b738ae66d
astro-claire <clairewilliams@g.ucla.edu>
2026-02-23 17:08:02 -0800

    Model updates including depletion

--- src/timescales/analysis/model.py
+++ src/timescales/analysis/model.py
@@ -12,21 +12,24 @@ from typing import Dict, Iterable, List, Literal, Optional, Tuple, Union
 import warnings
 from .tables import structural_table, timescale_table
 from .tools import get_system, condition_test
-from .integrals import Ncoll_pl_no_bh,Ncoll_pl_no_bh_limits,N_coll_bh_limits
+from .integrals import Ncoll_pl_no_bh,Ncoll_pl_no_bh_limits,N_coll_bh_limits,Mdot_pl_no_bh_limits, Mdot_binaries_pl_limits, Mdot_deplete_noBH_limits
 from .recipes import per_system_comparison, destructive_colllision_criterion
 from ..physics.stars import main_sequence_lifetime_approximation, stellar_radius_approximation  
 from ..physics.halo_environment import local_merger_timescale, neighbor_merger_timescale, interaction_timescale
 from ..physics.collisions import collision_timescale
 from ..utils.energy import escape_velocity
 from ..utils.filtering import filter_kwargs_for
-
+from .tools import select_coulomb_calculator
 
 def create_dynamical_model_integral(ensemble,*,
                         deltamstar = 0.5*u.Msun,
                         as_: Literal["dict", "pandas"] = "dict",
                         verbose = True, 
                         z_final = 0,
-                        mass_fraction_retained = .01
+                        mass_fraction_retained = .01, 
+                        mass_accretion_ratio = 0.5,
+                        f_vms = 2e-2, 
+                        t_dist_cc = None
                             ):
     """ 
     Create dynamical model using exact integral
@@ -65,6 +68,11 @@ def create_dynamical_model_integral(ensemble,*,
         all_possible_kwargs['int_type'] = 'neighbor'
         if verbose ==True:
             print("Using nearest neighbor for interaction type.")
+    if 'Z' not in ensemble.imf_kwargs:
+        if verbose ==True:
+            print("No Metallicity given! Using 0.1 solar")
+        ensemble.imf_kwargs['Z']=0.1
+    coulomb_func = select_coulomb_calculator(ensemble)
 
     #quantities that only need to be calculated once
     t_universe = all_possible_kwargs['cosmology'].age(z_final).to('yr')
@@ -83,6 +91,11 @@ def create_dynamical_model_integral(ensemble,*,
         out['t_merger'].append(t_merger)
         #find out what's the limiting time for the system:
         minimum_disruption_time.append(min([t_merger,t_ms,t_universe]))
+        if t_dist_cc is not None: 
+            tsc = get_system(timescales_by_radius, sys_id)
+            t_relax = tsc['t_relax'][-1]
+            minimum_disruption_time.append(t_relax*0.2)
+            print(t_relax)
         which_disruption_time.append([t_merger,t_ms,t_universe].index(min([t_merger,t_ms,t_universe])))
     out['which_disruption_time']=which_disruption_time
     out['minimum_disruption_time']= minimum_disruption_time
@@ -93,12 +106,17 @@ def create_dynamical_model_integral(ensemble,*,
     print("collisions occur in "+str(len(coll_sys_id))+" systems")
     #output empty lists
     out['N_collisions'] = [0]* ensemble.Nsystems
+    out['rho0'] = [0]* ensemble.Nsystems
+    out['M_VMS'] = [0]* ensemble.Nsystems #following Paccuci et al for this
     out['mass_fraction_retained'] = [0]* ensemble.Nsystems
     out['N_collisions_massloss']= [0]* ensemble.Nsystems
     out['fraction_sticky'] =[0]* ensemble.Nsystems
     out['N_collisions_df'] =[0]* ensemble.Nsystems # number of collisions in the dynamical friction region
     out['N_collisions_df_massloss'] =[0]* ensemble.Nsystems # number of collisions in the dynamical friction + massloss region
     out['mass_accretion_rate']=[0] * ensemble.Nsystems
+    out['mass_df_rate']=[0] * ensemble.Nsystems
+    out['mass_depletion_rate']=[0] * ensemble.Nsystems
+    out['mass_binaries_rate']=[0] * ensemble.Nsystems
     if 'Mcollisions' not in ensemble.timescales_kwargs.keys():
         ensemble.timescales_kwargs['Mcollisions'] = 1.0*u.Msun
 
@@ -114,6 +132,7 @@ def create_dynamical_model_integral(ensemble,*,
         GMR2 = c.G*ensemble.timescales_kwargs['Mcollisions']**2/(Rcollisions)
         out['mass_fraction_retained'][sys_id] = (reduced_mass *sigma_avg**2/(GMR1+GMR2)).cgs.value
         prof = ensemble.profiles[sys_id]
+        out['rho0'][sys_id]= prof.rho0
         cv = prof.get_veldisp_constant()
         ts = minimum_disruption_time[sys_id]
         if "BH" in ensemble.densityModel:
@@ -175,6 +194,10 @@ def create_dynamical_model_integral(ensemble,*,
         sticky_tdf= sys_df['stickytdf'] * u.yr
         newts = min(ts, main_sequence_lifetime_approximation(2*ensemble.Mstar))
         where_stickydf = np.where(sticky_tdf<newts)[0]
+        relaxt= sys_df['t_relax'] * u.yr
+        where_relax = np.where(relaxt>10*u.yr)[0]
+        r_relax = ensemble.radii[sys_id][where_relax[0]]
+        # print("relaxr", r_relax)
         if len(where_stickydf>1):
             out['fraction_sticky'][sys_id] = float(where_stickydf[-1])/ensemble.Nsampling
             r_stickydf = ensemble.radii[sys_id][where_stickydf[-1]] #rmax of dynamical friction region
@@ -203,6 +226,54 @@ def create_dynamical_model_integral(ensemble,*,
                                         rmax =r_stickydf,
                                         rmin =rmin,
                                         e = ensemble.timescales_kwargs["e"])        
+                sys_data = get_system(denclosedmass_byradius, sys_id)
+                coulomb_log = coulomb_func(sys_data['Menc'][-1],sys_data['r'][-1],sys_data["sigma"][-1], Mstar = ensemble.timescales_kwargs["Mstar"])
+                # print((reduced_mass*np.array(sys_data["sigma"]*(u.km/u.s))**2/(GMR1+GMR2)).cgs)
+                out['mass_df_rate'][sys_id] = mass_accretion_ratio* Mdot_pl_no_bh_limits(prof.r0,
+                                        newts, 
+                                        prof.alpha, 
+                                        cv,
+                                        prof.rho0,
+                                        f_IMF_m,
+                                        reduced_mass, 
+                                        coulomb_log,
+                                        Mstar = ensemble.timescales_kwargs["Mstar"],#1.0*u.Msun,
+                                        Mcollisions=ensemble.timescales_kwargs['Mcollisions'], 
+                                        rmax =r_stickydf,
+                                        rmin =r_relax,
+                                        e = ensemble.timescales_kwargs["e"]).to(u.Msun/u.yr)
+                out['mass_binaries_rate'][sys_id]= Mdot_binaries_pl_limits(prof.r0,
+                                        newts, 
+                                        prof.alpha, 
+                                        cv,
+                                        prof.rho0,
+                                        f_IMF_m,
+                                        reduced_mass, 
+                                        coulomb_log,
+                                        prof.enclosed_mass(r_stickydf),
+                                        r_stickydf,
+                                        Mstar = ensemble.timescales_kwargs["Mstar"],#1.0*u.Msun,
+                                        Mcollisions=ensemble.timescales_kwargs['Mcollisions'], 
+                                        rmax =r_stickydf,
+                                        rmin =r_relax,
+                                        e = ensemble.timescales_kwargs["e"]).to(u.Msun/u.yr)
+                out['mass_depletion_rate'][sys_id] = Mdot_deplete_noBH_limits(prof.r0,
+                                        newts, 
+                                        prof.alpha, 
+                                        cv,
+                                        prof.rho0,
+                                        f_IMF_m,
+                                        reduced_mass, 
+                                        coulomb_log,
+                                        Mstar = ensemble.timescales_kwargs["Mstar"],#1.0*u.Msun,
+                                        Mcollisions=ensemble.timescales_kwargs['Mcollisions'], 
+                                        rmax =r_stickydf,
+                                        rmin =r_relax,
+                                        e = ensemble.timescales_kwargs["e"]).to(u.Msun/u.yr)
+                if out['mass_depletion_rate'][sys_id]<((ensemble.timescales_kwargs["Mstar"]/(ensemble.timescales_kwargs["Mstar"]+ensemble.timescales_kwargs['Mcollisions'])).cgs*out['mass_df_rate'][sys_id]):
+                    out['mass_depletion_rate'][sys_id]= ((ensemble.timescales_kwargs["Mstar"]/(ensemble.timescales_kwargs["Mstar"]+ensemble.timescales_kwargs['Mcollisions'])).cgs*out['mass_df_rate'][sys_id])
+                out['mass_accretion_rate'][sys_id]= (1-f_vms)*(out['mass_df_rate'][sys_id]-out['mass_depletion_rate'][sys_id]-out['mass_binaries_rate'][sys_id])
+                # print(Mdottest.to(u.Msun/u.yr))
             if len(ml_idx)>1:
                 where_ml_cutoff = ml_idx[-1]
                 radiusml = sys_massloss['r'][where_ml_cutoff] 
@@ -231,17 +302,22 @@ def create_dynamical_model_integral(ensemble,*,
                                             e = ensemble.timescales_kwargs["e"],
                                             rmin =rmin,
                                             rmax = min(r_stickydf,radiusml))
-            if len(ml_idx)>1:
-                out['mass_accretion_rate'][sys_id] = (out['N_collisions_df'][sys_id]-out['N_collisions_df_massloss'][sys_id])* out['mass_fraction_retained'][sys_id]*ensemble.timescales_kwargs["Mstar"]/(sticky_tdf[where_stickydf][-1])
-            else:
-                out['mass_accretion_rate'][sys_id] = out['N_collisions_df'][sys_id]* out['mass_fraction_retained'][sys_id]*ensemble.timescales_kwargs["Mstar"]/(sticky_tdf[where_stickydf][-1])
-            # out['mass_accretion_rate'][sys_id] = out['N_collisions_df'][sys_id]* mass_fraction_retained*ensemble.timescales_kwargs["Mstar"]/(sticky_tdf[where_stickydf][-1])
+            
+            
+            # if len(ml_idx)>1:
+            #     out['mass_accretion_rate'][sys_id] = (out['N_collisions_df'][sys_id]-out['N_collisions_df_massloss'][sys_id])* out['mass_fraction_retained'][sys_id]*ensemble.timescales_kwargs["Mstar"]/(sticky_tdf[where_stickydf][-1])
+            # else:
+            #     out['mass_accretion_rate'][sys_id] = out['N_collisions_df'][sys_id]* out['mass_fraction_retained'][sys_id]*ensemble.timescales_kwargs["Mstar"]/(sticky_tdf[where_stickydf][-1])
+            # # out['mass_accretion_rate'][sys_id] = out['N_collisions_df'][sys_id]* mass_fraction_retained*ensemble.timescales_kwargs["Mstar"]/(sticky_tdf[where_stickydf][-1])
             if out['mass_accretion_rate'][sys_id]==0:
-                out['mass_accretion_rate'][sys_id]= 0 *u.Msun/u.yr
+                out['mass_accretion_rate'][sys_id]= 0 *u.Msun/u.yr 
+            Z = ensemble.imf_kwargs['Z']
+            out['M_VMS'][sys_id]= (out['mass_accretion_rate'][sys_id].to_value(u.Msun/u.yr)/(10**(-9.13)*Z**0.74))**(1./2.1)* u.Msun
     whereml, = np.where(np.array(out['N_collisions_massloss'])>1)
     print("mass loss occurs in "+str(len(whereml))+" systems")
     print(len(np.where(np.array(out['N_collisions_df_massloss'])>1)[0]))
-    print(out['mass_fraction_retained'])
+    print(out['M_VMS'])
+    print(out['mass_accretion_rate'])
     out['fraction_collisions_df']= np.array(out['N_collisions_df'])/np.array(out['N_collisions'])
     out['N_collisions_constructive'] = np.array(out['N_collisions_df'])-np.array(out['N_collisions_df_massloss'])
     superstar= np.array([out['N_collisions_constructive'][i] * out['mass_fraction_retained'][i] for i in range(len(out['N_collisions_constructive']))])
