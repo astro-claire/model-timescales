e9de9716c3ee56d484cb1dfb616da54dd06b66b2
astro-claire <clairewilliams@g.ucla.edu>
2026-02-28 14:53:48 -0800

    added relaxation radii limits

--- src/timescales/physics/relaxation.py
+++ src/timescales/physics/relaxation.py
@@ -13,3 +13,26 @@ def relaxation_timescale(v,rho, mass, coulomb=10, v_unit = u.km/u.s, rho_unit = 
     mass = as_quantity(mass, mass_unit)
 
     return (0.34 * v**3 / (c.G**2 * rho * mass *coulomb)).to('yr')
+
+
+
+def r_no_relax(rho0,r0, Mstar, coulomb, cv, alpha):
+    """ 
+    Radius at which
+    Relaxation time = orbital period
+    """
+    num = 8. *np.pi * 0.34 * cv**(3./2.) *rho0 * r0**alpha
+    denom = (1.+alpha)**(3./2.)* (3.-alpha)**2. * Mstar * coulomb
+
+    return ((num/denom)**(1./(alpha-3.))).to('pc')
+
+def r_no_relax_bh(rho0,r0, Mstar, coulomb, cv, alpha, MBH):
+    """ 
+    Radius at which
+    Relaxation time = orbital period
+    Assuming you're inside the BH sphere of influence
+    """
+    box = 0.34 * cv**(3./2.) / 2. / np.pi / (1.+alpha)**(3./2.) / Mstar  / coulomb
+    num = box * MBH**2 
+    denom = rho0*r0**alpha
+    return ((num/denom)**(1./(3.-alpha))).to('pc')
